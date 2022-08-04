47999e762feb41ce640f67c6032ae341afc67a8e
AndresTapiero <andrestapiero72@gmail.com>
2022-08-03 22:59:58 -0500

    Change text to constants

--- app/src/main/java/com/andrest/databaseimplementation/api/APIService.kt
+++ app/src/main/java/com/andrest/databaseimplementation/api/APIService.kt
@@ -2,15 +2,16 @@ package com.andrest.databaseimplementation.api
 
 import com.andrest.databaseimplementation.models.Post
 import com.andrest.databaseimplementation.models.User
+import com.andrest.databaseimplementation.rest.EndPoints
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface APIService {
 
-    @GET("/users")
+    @GET(EndPoints.GET_USERS)
     fun getAllUsers(): Call<List<User>>
 
-    @GET("/posts?")
+    @GET(EndPoints.GET_POST_USER)
     fun getUserPosts(@Query("userId") id: Int): Call<List<Post>>
 }
